727801aca4eac1b4a4a95adf388ed582df53410f
Reid Poole <poole.reid@gmail.com>
2022-09-21 22:32:47 -0500

    Add Console

--- comparisons.js
+++ comparisons.js
@@ -89,15 +89,15 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
-
+console.log(lovesToPlay && age);
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER:The final line of code evaluated to "1" because Puppy isnt and option but 1 is the age of the puppy.
